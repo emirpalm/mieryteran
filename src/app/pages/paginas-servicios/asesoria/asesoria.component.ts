fd452c126cdd404ebcf12a2e564a8b3efa27f638
emirpalm <emirpalm@outlook.com>
2018-05-07 13:41:44 -0500

    paginas

---
+++ src/app/pages/paginas-servicios/asesoria/asesoria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asesoria',
+  templateUrl: './asesoria.component.html',
+  styles: []
+})
+export class AsesoriaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
