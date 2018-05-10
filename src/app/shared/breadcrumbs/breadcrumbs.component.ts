aa8c0c3934daa53d18b39a92cbae98225b5eac6a
emirpalm <emirpalm@outlook.com>
2018-05-10 12:50:19 -0500

    cambios en breadcrums.component.ts metatags,filters,map

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  label = '';
+  constructor(
+    private router: Router,
+    public title: Title,
+    public meta: Meta
+  ) {
 
+this.getDataRouter()
+    .subscribe( data => {
+
+       console.log (data );
+
+       this.label = data.titulo;
+       this.title.setTitle(this.label);
+       // tslint:disable-next-line:prefer-const
+       let metaTag: MetaDefinition = {
+       name: 'description',
+       content: this.label
+       };
+       this.meta.updateTag(metaTag);
+      });
+}
   ngOnInit() {
   }
-
+getDataRouter() {
+    return this.router.events
+    .filter( evento => evento instanceof ActivationEnd )
+    .filter( (evento: ActivationEnd) => evento.snapshot.firstChild === null )
+    .map( (evento: ActivationEnd) => evento.snapshot.data);
 }
+ }
